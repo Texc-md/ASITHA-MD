863625642ac0ec0cbbc052638326f3a08d4d55d3
Texc-md <techmd00009@gmail.com>
2024-10-15 00:13:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=viYCBDYR#7Z7qe-w6c3EJbm2049hWnwJjpweTaeIoUZ_jqDZsmqY",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
